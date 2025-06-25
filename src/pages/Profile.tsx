71f0aa442cc01f9b0a6cd2147d01e66b5b61b1ac
Nora7980 <146598405+Nora7980@users.noreply.github.com>
2025-06-25 02:29:27 -0700

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,29 +1,27 @@
 // src/pages/Profile.tsx
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
+import { useApi } from "../services/api";
 
 const App = () => {
   const [data, setData] = useState(null);
-  const [error, setError] = useState<string | null>(null);
-
+  const [error, setError] = useState<string | null>(null);  
+  const api = useApi();
+  const fetchdata = useCallback(async () => {
+    try {
+      const response = await api.get('https://ue3b2jb6robht6kl62ekulzh7y0dcdrw.lambda-url.us-east-1.on.aws/'); // Example GET URL
+      console.log(response);
+      setData(response.data);
+    } catch (err: any) {
+      console.log("get request failed");
+      return;
+    }
+  }, [api]);
   useEffect(() => {
-    fetch('https://ue3b2jb6robht6kl62ekulzh7y0dcdrw.lambda-url.us-east-1.on.aws/') // Example GET URL
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        setData(json);
-      })
-      .catch(err => {
-        setError(err.message);
-      });
-  }, []);
-
+    fetchdata();
+  }, [fetchdata]);
   return (
     <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-      <h1>Fetch API GET Request Example</h1>
+      <h1>Your Information</h1>
       {error && <p style={{ color: 'red' }}>Error: {error}</p>}
       {data ? (
         <pre>{JSON.stringify(data, null, 2)}</pre>
