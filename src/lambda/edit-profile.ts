86c8db71580eabdd017b8f244e8f19bfcd99f832
Nora7980 <146598405+Nora7980@users.noreply.github.com>
2025-07-08 15:27:22 -0700

    Update edit-profile.ts

--- src/lambda/edit-profile.ts
+++ src/lambda/edit-profile.ts
@@ -139,15 +139,15 @@ export const handler = async (
     const { name, school, subject } = info;
 
     if (typeof name === "string" && name.trim() !== "") {
-        await db.query("UPDATE users SET name = " + name + " WHERE email = " + decoded.email)
+        await db.query("UPDATE users SET name = " + name + " WHERE email = $1", [decoded.email])
     }
 
     if (typeof school === "string" && school.trim() !== "") {
-        await db.query("UPDATE users SET school = " + school + " WHERE email = " + decoded.email)
+        await db.query("UPDATE users SET school = " + school + " WHERE email = $1", [decoded.email])
     }
 
     if (typeof subject === "string" && subject.trim() !== "") {
-        await db.query("UPDATE users SET subject = " + subject + " WHERE email = " + decoded.email)
+        await db.query("UPDATE users SET subject = " + subject + " WHERE email = $1", [decoded.email])
     }
 
     return {
