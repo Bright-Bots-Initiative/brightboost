37a0fa8988c3a686c4267c2086e21ee3e8f00d46
Nora7980 <146598405+Nora7980@users.noreply.github.com>
2025-07-16 11:13:23 -0700

    Update EditProfile.tsx
    Adding conditions to the form so that teacher can edit bio while students can't

--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -5,16 +5,18 @@ import { useApi } from "../services/api";
 const Edit = () => {
   const api = useApi();
   
-  const [profileUrl, setProfileUrl] = useState("DEFAULT PROFILE URL");
+  const [profileUrl, setProfileUrl] = useState("https://api.dicebear.com/7.x/identicon/svg?seed=default");
+  const [role, setRole] = useState("blank");
   
   useEffect(() => {
     const fetchProfile = async () => {
       try {
-        const user_data = await api.get("/profile");
+        const user_data = await api.get("/api/profile");
         const parsed = JSON.parse(user_data);
         setProfileUrl(parsed.avatar);
+        setRole(parsed.role)
       } catch (err: any) {
-        console.log("Failed to get image");
+        console.log("Failed to get image/role");
       }
     };
 
@@ -28,16 +30,20 @@ const Edit = () => {
       const name = formData.get("name");
       const school = formData.get("school");
       const subject = formData.get("subject");
+      const bio = role === "teacher" ? formData.get("bio") : "blank";
       const data = {
+        role: role,
         name: name,
         school: school,
         subject: subject,
+        bio: bio
       };
       const response = await api.post("/api/edit-profile", data);
       console.log(response);
     },
-    [api],
+    [api, role],
   );
+    
   return (
     <>
     <img 
@@ -45,9 +51,12 @@ const Edit = () => {
       alt="profile picture"
       />
     <form onSubmit={edit}>
-      <input name="name" />
-      <input name="school" />
-      <input name="subject" />
+      <input name="name" placeholder="Name" />
+      <input name="school" placeholder="School" />
+      <input name="subject" placeholder="Subject" />
+      {role === "teacher" && (
+        <input name="bio" placeholder="Bio" />
+      )}
       <button type="submit">Save</button>
     </form>
     </>
