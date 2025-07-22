4725a3958c52d497736fbbf21c83477c906eed6b
Louis Albanez <albanezla@uchicago.edu>
2025-07-22 17:59:41 -0400

    disable teacher navlinks that lead to 404 pages

--- src/components/TeacherDashboard/Sidebar.tsx
+++ src/components/TeacherDashboard/Sidebar.tsx
@@ -13,20 +13,29 @@ const Sidebar: React.FC = () => {
     <div className="w-64 h-screen bg-gray-800 text-white p-6 space-y-4 fixed top-0 left-0 shadow-lg">
       <h2 className="text-2xl font-semibold mb-6 text-center">Teacher Admin</h2>
       <nav>
-        {navItems.map(({ name, path }) => (
-          <NavLink
-            key={name}
-            to={path}
-            className={({ isActive }) =>
-              `block py-3 px-4 rounded-lg transition duration-200 ease-in-out text-sm font-medium
+        {navItems.map(({ name, path }) =>
+          ["Students", "Settings"].includes(name) ? (
+            <p
+              className="block py-3 px-4 rounded-lg text-sm font-medium
+              text-gray-500"
+            >
+              {name}
+            </p>
+          ) : (
+            <NavLink
+              key={name}
+              to={path}
+              className={({ isActive }) =>
+                `block py-3 px-4 rounded-lg transition duration-200 ease-in-out text-sm font-medium
               hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 ${
                 isActive ? "bg-blue-600 text-white shadow-md" : "text-gray-300"
               }`
-            }
-          >
-            {name}
-          </NavLink>
-        ))}
+              }
+            >
+              {name}
+            </NavLink>
+          ),
+        )}
       </nav>
     </div>
   );
